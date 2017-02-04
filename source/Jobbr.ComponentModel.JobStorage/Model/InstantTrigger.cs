1c90fb50e65c2d066039d7d02a3383c3c0432776
Michael Schnyder <michael.schnyder@outlook.com>
2017-02-04 13:50:47 +0100

    missing property

--- source/Jobbr.ComponentModel.JobStorage/Model/InstantTrigger.cs
+++ source/Jobbr.ComponentModel.JobStorage/Model/InstantTrigger.cs
@@ -7,5 +7,6 @@ namespace Jobbr.ComponentModel.JobStorage.Model
     {
         public int DelayedMinutes { get; set; }
         public string Comment { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
     }
 }
