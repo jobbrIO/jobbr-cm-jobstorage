df98c19fd845ea953d291e33fe83a50ff8f3a6a1
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-04-04 11:18:33 +0200

    enum values defined

--- source/Jobbr.ComponentModel.JobStorage/Model/JobRunStates.cs
+++ source/Jobbr.ComponentModel.JobStorage/Model/JobRunStates.cs
@@ -8,71 +8,71 @@ namespace Jobbr.ComponentModel.JobStorage.Model
         /// <summary>
         /// The null :)
         /// </summary>
-        Null,
+        Null = 0,
 
         /// <summary>
         /// The JoBRun is queued unless a JobStarter starts a new executable
         /// </summary>
-        Scheduled,
+        Scheduled = 1,
 
         /// <summary>
         /// The JobStarter has created a enviornment for the Job and copies a related files/data to the working directory
         /// </summary>
-        Preparing,
+        Preparing = 2,
 
         /// <summary>
         /// The JobStarted has started a new executable 
         /// </summary>
-        Starting,
+        Starting = 3,
 
         /// <summary>
         /// The JobStarted has created a new environment and the executable has been started
         /// </summary>
-        Started,
+        Started = 4,
 
         /// <summary>
         /// The Executable itself has connected to the jobServer
         /// </summary>
-        Connected,
+        Connected = 5,
 
         /// <summary>
         /// The Executable is running and connected to the jobserver
         /// </summary>
-        Initializing,
+        Initializing = 6,
 
         /// <summary>
         /// The logic has started to run
         /// </summary>
-        Processing,
+        Processing = 7,
 
         /// <summary>
         /// The external code was run
         /// </summary>
-        Finishing,
+        Finishing = 8,
 
         /// <summary>
         /// Collecting the files
         /// </summary>
-        Collecting,
+        Collecting = 9,
 
         /// <summary>
         /// The job as executed sucessfully and the executer has cleaned up and terminated
         /// </summary>
-        Completed,
+        Completed = 10,
 
         /// <summary>
         /// The job failed.
         /// </summary>
-        Failed,
+        Failed = 11,
 
         /// <summary>
         /// The JobRun has ben deleted in advance
         /// </summary>
-        Deleted,
+        Deleted = 12,
 
         /// <summary>
         /// The JobRun has been omitted. Eg job has been scheduled, Jobserver stopped (before the jobrun is executed) and After PlannedStartDateTime started again -> JobRun won't be started in that case but set to Omitted.
         /// </summary>
-        Omitted
+        Omitted = 13
     }
 }
